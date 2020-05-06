6c1f7868279cf17e920df9fbdab8f8117924b1be
Bruno Marchi <64336373+brunomarchir@users.noreply.github.com>
2020-05-06 13:56:40 -0400

    Added Search Bar (1st attempt)

--- js/script.js
+++ js/script.js
@@ -1,59 +1,103 @@
 
 
 //get the DOM elements
-
-let list = document.querySelector('.student-list');
-let studentItem = list.children;
-divPage = document.querySelector('.page');
+const studentList = document.getElementsByClassName('student-item cf');
+const studentItem = studentList.children;
 const numItems = 10;
+const divPage = document.querySelector('.page');
+const pagination = document.createElement('div');
+pagination.className = ("pagination");
 
+// this function limits how many items will be shown each page
 
-// this function limits how many pages will be shown each page
-
-var showPage = (page) => {
+var showPage = (list, page) => {
 
   let indexFirst = (page * numItems) - numItems;
-  let indexLast = (page * numItems) - 1;
+  let indexLast  = (page * numItems) - 1;
 
-  for (let i = 0; i < list.childElementCount; i++) { //the "childElementCount" property was extracted from https://www.w3schools.com/jsref/prop_element_childelementcount.asp
+  for (let i = 0; i < list.length; i++) {
     if (i >= indexFirst && i <= indexLast) {
-      list.children[i].style.display = ('block');
+      list[i].style.display = ('block');
     }
       else {
-        list.children[i].style.display = ('none');
+        list[i].style.display = ('none');
       }
   }
 };
 
-//call function for the initial page
-showPage(1);
+//function to reset pagination for a different list
+const reset = () => {
+   pagination.innerHTML = '';
+}
+
+//calls function for the initial page
+showPage(studentList, 1);
 
 // create and add the links for the pagination
+function appendPageLinks(list) {
 
-function appendPageLinks() {
-  var numPages = Math.ceil(list.childElementCount/10); //gets the number of pages needed
-  let pagination = document.createElement('div');
+  let numPages = Math.ceil(list.length/10); //gets the number of pages needed
   let ul = document.createElement('ul');
-  pagination.className = ("pagination");
 
   divPage.appendChild(pagination);
   pagination.appendChild(ul);
-
-
 
   for (let i=1; i <= numPages; i++) {
     let li = document.createElement('li');
     let link = document.createElement('a');
     ul.appendChild(li);
     li.appendChild(link);
+    link.href = '#';
     link.textContent = i;
 
 //add funcionality to the page's buttons
-
-    link.addEventListener('click', () => {
-      showPage(i);
+    link.addEventListener('click', (e) => {
+      showPage(list, i);
     })
   }
 };
 
-appendPageLinks();
+//call function for the pagination of the students list
+appendPageLinks(studentList);
+
+
+//student search
+const divSearch = document.createElement('div');
+const input = document.createElement('input');
+const searchButton = document.createElement('button');
+const studentName = document.getElementsByTagName('h3');
+const filteredList = [];
+divSearch.className = ('student-search');
+input.placeholder = "Search for students...";
+searchButton.textContent = 'Search';
+
+document.querySelector('.page-header').appendChild(divSearch);
+divSearch.appendChild(input);
+divSearch.appendChild(searchButton);
+
+//function filters the list and hides the rest of the items
+function searchStudent() {
+  for (let x = 0; x < studentList.length; x++) {
+    if (studentName[x].innerHTML.includes(input.value.toLowerCase())) {
+      studentList[x].style.display = ('block')
+      filteredList.push(studentList[x].parentNode.parentNode);
+
+    }
+      else {
+        studentList[x].style.display = ('none');
+
+      }
+  }
+
+  reset();
+  console.log(filteredList.length);
+  showPage(filteredList, 1);
+  appendPageLinks(filteredList);
+
+};
+
+
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchStudent();
+});
